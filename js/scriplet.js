695acf721f94973ef625626ddc1d4823a3b1e787
Manthan Pandey <manthanpandey23@gmail.com>
2020-10-01 10:01:38 +0530

    Add files in separate folder

---
+++ js/scriplet.js
@@ -0,0 +1,19 @@
+// Absolutely useless nonsense viewing script by MS
+var previousJoke = '';
+jQuery(document).ready(function() {
+	getJoke();
+	// update every 10 min
+	setInterval(getJoke, 10 * 60 * 1000);
+});
+
+// Assign joke html
+function getJoke() {
+	jQuery.get("../files/fetch_mails.php", function(joke) {
+		if (previousJoke !== joke) {
+			jQuery("#lame_joke").css('opacity', '0');
+		  	jQuery("#lame_joke").html(joke).animate({opacity: 1}, 500);
+			previousJoke = joke;
+			//alert(joke);
+		}
+	});
+}
